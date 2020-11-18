8ab686276026c7bb64657fe4384ac47be99b6b45
Andrei Kudrin <andreikudrin@gmail.com>
2020-11-18 11:28:35 +0000

    Adding Collection Document Validation

--- MongoDB Recipes/CH2/readOperations.js
+++ MongoDB Recipes/CH2/readOperations.js
@@ -1,8 +1,11 @@
 //Use the following syntax to retrieve documents from a collection.
-db.collection.find()
-db.person.find({})
-db.person.find({name:"shobana"})
-db.person.find({age:{$gt:10}})
-db.person.find({ name:"shobana",age:{$gt:10}})
-db.person.find( { $or: [ { name: "shobana" }, { age: { $eq: 20
-} } ] } )
+db.collection.find();
+db.person.find({});
+db.person.find({ name: "shobana" });
+db.person.find({ age: { $gt: 10 } });
+db.person.find({ name: "shobana", age: { $gt: 10 } });
+db.person.find({ $or: [{ name: "shobana" }, { age: { $eq: 20 } }] });
+db.employeedetails.find({ projects: ["Hadoop", "MongoDB"] });
+db.employeedetails.find({ projects: "MongoDB" });
+db.employeedetails.find({ scores: { $gt: 26 } });
+db.employeedetails.find({ scores: { $gt: 20, $lt: 24 } });
